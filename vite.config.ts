4c8fda1c751fefe9bd93d0ef342056e8a6a49294
BoschLeith <bosch.leith@gmail.com>
2025-02-04 16:10:30 -0600

    Remove Tailwind v4; Add Shadcn/ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-    },
-  },
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
     proxy: {
       '/api': 'http://localhost:8080',
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
